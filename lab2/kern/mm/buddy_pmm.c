facc81e03b9fcfb8875177a6f735f95cde408cb6
whileskies <gt110@qq.com>
2020-10-09 22:27:13 +0800

    update lab2 challenge1

--- lab2/kern/mm/buddy_pmm.c
+++ lab2/kern/mm/buddy_pmm.c
@@ -1,21 +1,21 @@
 #include <pmm.h>
 #include <buddy_pmm.h>
 
-size_t buddy_size;
-size_t *longest;
-size_t free_size;
-struct Page *mem_begin_page;
 
 struct buddy {
     size_t size;
     uintptr_t *longest;
+    size_t longest_num_page;
+    size_t total_num_page;
     size_t free_size;
     struct Page *begin_page;
-    list_entry_t link;
 };
 
-struct buddy mem_buddy_link;
+struct buddy mem_buddy[MAX_NUM_BUDDY_ZONE];
+int num_buddy_zone = 0;
 
+static void
+debug_buddy_tree(int level, char *label);
 
 static size_t next_power_of_2(size_t size) {
     size |= size >> 1;
@@ -28,32 +28,55 @@ static size_t next_power_of_2(size_t size) {
 
 static void
 buddy_init() {
-    list_init(&mem_buddy_link.link);
+
 }
 
 static void
 buddy_init_memmap(struct Page *base, size_t n) {
-    struct buddy buddy;
-    buddy.longest = page2ka(base);
-    
-    buddy.begin_page = pa2page(PADDR(ROUND_UP(buddy.longest + 2 * n * sizeof(uintptr_t), PGSIZE)));
-    buddy_free_size = buddy.begin_page - base;
-    
+    cprintf("n: %d\n", n);
+    struct buddy *buddy = &mem_buddy[num_buddy_zone++];
 
-    mem_begin_page = base;
-    buddy_size = next_power_of_2(n) / 2;
-    free_size = buddy_size;
-    size_t node_size = buddy_size * 2;
-    
-    for (int i = 0; i < 2 * buddy_size - 1; i++) {
+    size_t v_size = next_power_of_2(n);
+    size_t excess = v_size - n;
+    size_t v_alloced_size = next_power_of_2(excess);
+
+    buddy->size = v_size;
+    buddy->free_size = v_size - v_alloced_size;
+    buddy->longest = page2kva(base);
+    buddy->begin_page = pa2page(PADDR(ROUNDUP(buddy->longest + 2 * v_size * sizeof(uintptr_t), PGSIZE)));
+    buddy->longest_num_page = buddy->begin_page - base;
+    buddy->total_num_page = n - buddy->longest_num_page;
+
+    //cprintf("base: %p, longest: %p, begin_page: %p, free_size: %d\n", page2kva(base), buddy->longest, page2kva(buddy->begin_page), buddy->free_size);
+
+    //buddy->size = next_power_of_2(buddy->total_num_page) / 2;
+    //buddy->free_size = buddy->size;
+
+    size_t node_size = buddy->size * 2;
+
+    for (int i = 0; i < 2 * buddy->size - 1; i++) {
         if (IS_POWER_OF_2(i + 1)) {
             node_size /= 2;
         }
-        longest[i] = node_size;
+        buddy->longest[i] = node_size;
     }
 
-    struct Page *p = base;
-    for (; p != base + buddy_size; p ++) {
+    int index = 0;
+    while (1) {
+        if (buddy->longest[index] == v_alloced_size) {
+            buddy->longest[index] = 0;
+            break;
+        }
+        index = RIGHT_LEAF(index);
+    }
+
+    while (index) {
+        index = PARENT(index);
+        buddy->longest[index] = MAX(buddy->longest[LEFT_LEAF(index)], buddy->longest[RIGHT_LEAF(index)]);
+    }
+
+    struct Page *p = buddy->begin_page;
+    for (; p != base + buddy->free_size; p ++) {
         assert(PageReserved(p));
         p->flags = p->property = 0;
         set_page_ref(p, 0);
@@ -65,67 +88,86 @@ buddy_alloc_pages(size_t n) {
     assert(n > 0);
     if (!IS_POWER_OF_2(n))
         n = next_power_of_2(n);
-    
+
     size_t index = 0;
     size_t node_size;
     size_t offset = 0;
 
-    if (longest[index] < n) {
+    struct buddy *buddy = NULL;
+    for (int i = 0; i < num_buddy_zone; i++) {
+        if (mem_buddy[i].longest[index] >= n) {
+            buddy = &mem_buddy[i];
+            break;
+        }
+    }
+
+    if (!buddy) {
         return NULL;
     }
 
-    for (node_size = buddy_size; node_size != n; node_size /= 2) {
-        if (longest[LEFT_LEAF(index)] >= n) 
+    for (node_size = buddy->size; node_size != n; node_size /= 2) {
+        if (buddy->longest[LEFT_LEAF(index)] >= n)
             index = LEFT_LEAF(index);
-        else 
+        else
             index = RIGHT_LEAF(index);
     }
 
-    longest[index] = 0;
-    offset = (index + 1) * node_size - buddy_size;
+    buddy->longest[index] = 0;
+    offset = (index + 1) * node_size - buddy->size;
 
     while (index) {
         index = PARENT(index);
-        longest[index] = MAX(longest[LEFT_LEAF(index)], longest[RIGHT_LEAF(index)]);
+        buddy->longest[index] = MAX(buddy->longest[LEFT_LEAF(index)], buddy->longest[RIGHT_LEAF(index)]);
     }
 
-    free_size -= n;
+    buddy->free_size -= n;
 
-    return mem_begin_page + offset;
+    return buddy->begin_page + offset;
 }
 
 
 static void
 buddy_free_pages(struct Page *base, size_t n) {
+    struct buddy *buddy = NULL;
+
+    for (int i = 0; i < num_buddy_zone; i++) {
+        struct buddy *t = &mem_buddy[i];
+        if (base >= t->begin_page && base < t->begin_page + t->size) {
+            buddy = t;
+        }
+    }
+
+    if (!buddy) return;
+
     unsigned node_size, index = 0;
     unsigned left_longest, right_longest;
-    unsigned offset = base - mem_begin_page;
+    unsigned offset = base - buddy->begin_page;
 
-    assert(offset >= 0 && offset < buddy_size);
+    assert(offset >= 0 && offset < buddy->size);
 
     node_size = 1;
-    index = offset + buddy_size - 1;
+    index = offset + buddy->size - 1;
 
-    for (; longest[index]; index = PARENT(index)) {
+    for (; buddy->longest[index]; index = PARENT(index)) {
         node_size *= 2;
         if (index == 0)
             return;
     }
 
-    longest[index] = node_size;
-    free_size += node_size;
+    buddy->longest[index] = node_size;
+    buddy->free_size += node_size;
 
     while (index) {
         index = PARENT(index);
         node_size *= 2;
 
-        left_longest = longest[LEFT_LEAF(index)];
-        right_longest = longest[RIGHT_LEAF(index)];
+        left_longest = buddy->longest[LEFT_LEAF(index)];
+        right_longest = buddy->longest[RIGHT_LEAF(index)];
 
         if (left_longest + right_longest == node_size)
-            longest[index] = node_size;
-        else 
-            longest[index] = MAX(left_longest, right_longest);
+            buddy->longest[index] = node_size;
+        else
+            buddy->longest[index] = MAX(left_longest, right_longest);
     }
 
 }
@@ -133,49 +175,114 @@ buddy_free_pages(struct Page *base, size_t n) {
 
 static size_t
 buddy_nr_free_pages(void) {
-    return free_size;
+    size_t total_free_pages = 0;
+    for (int i = 0; i < num_buddy_zone; i++) {
+        total_free_pages += mem_buddy[i].free_size;
+    }
+    return total_free_pages;
 }
 
 
 static void
 buddy_check(void) {
-    cprintf("buddy_size: %d\n", buddy_size);
-    for (int i = 0; i < 10; i++) {
-        cprintf("longest[%d] = %d\n", i, longest[i]);
-    }
+    size_t total = buddy_nr_free_pages();
+    cprintf("total: %d\n", total);
 
-    assert(buddy_nr_free_pages() == buddy_size);
     struct Page *p0 = alloc_page();
     assert(p0 != NULL);
-    assert(buddy_nr_free_pages() == buddy_size - 1);
-    assert(p0 == mem_begin_page);
+    assert(buddy_nr_free_pages() == total - 1);
+    assert(p0 == mem_buddy[0].begin_page);
 
     struct Page *p1 = alloc_page();
     assert(p1 != NULL);
-    assert(buddy_nr_free_pages() == buddy_size - 2);
-    assert(p1 == mem_begin_page + 1);
+    assert(buddy_nr_free_pages() == total - 2);
+    assert(p1 == mem_buddy[0].begin_page + 1);
+
+    assert(p1 == p0 + 1);
 
     buddy_free_pages(p0, 1);
     buddy_free_pages(p1, 1);
-    assert(buddy_nr_free_pages() == buddy_size);
+    assert(buddy_nr_free_pages() == total);
 
     p0 = buddy_alloc_pages(11);
-    assert(buddy_nr_free_pages() == buddy_size - 16);
+    assert(buddy_nr_free_pages() == total - 16);
 
     p1 = buddy_alloc_pages(100);
+    assert(buddy_nr_free_pages() == total - 144);
 
-    for (int i = 0; i < 10; i++) {
-        cprintf("longest[%d] = %d\n", i, longest[i]);
+    buddy_free_pages(p0, -1);
+    buddy_free_pages(p1, -1);
+    assert(buddy_nr_free_pages() == total);
+
+    p0 = buddy_alloc_pages(total);
+    assert(p0 == NULL);
+
+    // debug_buddy_tree(7, "221, init");
+    p0 = buddy_alloc_pages(512);
+    // debug_buddy_tree(7, "221, alloc 512");
+    assert(buddy_nr_free_pages() == total - 512);
+
+    p1 = buddy_alloc_pages(1024);
+    // debug_buddy_tree(7, "225, alloc 1024");
+    assert(buddy_nr_free_pages() == total - 512 - 1024);
+
+    struct Page *p2 = buddy_alloc_pages(2048);
+    // debug_buddy_tree(7, "229, alloc 2048");
+    assert(buddy_nr_free_pages() == total - 512 - 1024 - 2048);
+
+    struct Page *p3 = buddy_alloc_pages(4096);
+    // debug_buddy_tree(7, "233, alloc 4096");
+    assert(buddy_nr_free_pages() == total - 512 - 1024 - 2048 - 4096);
+
+    struct Page *p4 = buddy_alloc_pages(8192);
+    // debug_buddy_tree(7, "237, alloc 8192");
+    assert(buddy_nr_free_pages() == total - 512 - 1024 - 2048 - 4096 - 8192);
+
+    struct Page *p5 = buddy_alloc_pages(8192);
+    // debug_buddy_tree(7, "241, alloc 8192");
+    assert(buddy_nr_free_pages() == total - 512 - 1024 - 2048 - 4096 - 8192 - 8192);
+
+    buddy_free_pages(p0, -1);
+    buddy_free_pages(p1, -1);
+    buddy_free_pages(p2, -1);
+    buddy_free_pages(p3, -1);
+    buddy_free_pages(p4, -1);
+    buddy_free_pages(p5, -1);
+
+    assert(buddy_nr_free_pages() == total);
+
+}
+
+
+static void
+debug_buddy_tree(int level, char *label) {
+    cprintf("\ndebug buddy tree: %s\n", label);
+
+    int num = 1;
+    int index = 0;
+    for (int i = 0; i < level; i++) {
+        int cur_num = 0;
+        while (1) {
+            cprintf("%d ", mem_buddy[0].longest[index++]);
+            cur_num++;
+            if (cur_num == num) {
+                cprintf("\n");
+                cur_num = 0;
+                num *= 2;
+                break;
+            }
+        }
     }
+    cprintf("debug buddy tree end\n\n");
 }
 
 
 const struct pmm_manager buddy_pmm_manager = {
-    .name = "buddy_pmm_manager",
-    .init = buddy_init,
-    .init_memmap = buddy_init_memmap,
-    .alloc_pages = buddy_alloc_pages,
-    .free_pages = buddy_free_pages,
-    .nr_free_pages = buddy_nr_free_pages,
-    .check = buddy_check,
+        .name = "buddy_pmm_manager",
+        .init = buddy_init,
+        .init_memmap = buddy_init_memmap,
+        .alloc_pages = buddy_alloc_pages,
+        .free_pages = buddy_free_pages,
+        .nr_free_pages = buddy_nr_free_pages,
+        .check = buddy_check,
 };
